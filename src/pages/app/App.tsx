83998f6a63789d6f498b928ca1954fb36e663965
Wafflus <wafflusdev@example.com>
2022-12-14 12:28:06 +0000

    Add hero component to app page

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from 'modules/header';
+import { Hero } from 'modules/hero';
 import { AppStyled } from './styles';
 
 function App() {
   return (
     <AppStyled>
       <Header />
+      <Hero />
     </AppStyled>
   );
 }
